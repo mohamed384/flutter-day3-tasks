e60cb8089240cfea8e71767f5abe6110250acb3c
MohamedEzz <me7717180@gmail.com>
2023-06-15 04:04:50 +0300

    last

--- lib/Home.dart
+++ lib/Home.dart
@@ -4,7 +4,7 @@ import 'package:task1/QuestionsScreen.dart';
 
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({ super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:task1/Home.dart';
-import 'package:task1/LoginInfo.dart';
-import 'package:task1/ScoreScreen.dart';
-// import 'package:flutter_application_2/Home.dart';
-// import 'package:flutter_application_2/Login.dart';
-// import 'package:flutter_application_2/LoginInfo.dart';
-// import 'package:flutter_application_2/QuestionsScreen.dart';
-// import 'package:flutter_application_2/ScoreScreen.dart';
+import 'app_router.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(BreakingBadApp(
+    appRouter: AppRouter(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BreakingBadApp extends StatelessWidget {
+  final AppRouter appRouter;
+
+  const BreakingBadApp({Key? key, required this.appRouter}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter tasks',
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      // ),
-
-
-      // home: QuizScreen(),
-      // routes:{
-      //   '/results':(context) => ScoreScreen(),
-      // },
-
-
-      home: const HomePage(),
-      routes: {
-      '/logininfo': (context) => LoginInfo(),
-      '/results':(context) => ScoreScreen(),
-      },
+      onGenerateRoute: appRouter.generateRoute,
     );
   }
 }
-
